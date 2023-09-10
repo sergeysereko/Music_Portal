106d9c0537d3f5e5939f43152dc09ad97da48f27
sergey <sergeysereko@gmail.com>
2023-09-10 22:59:52 +0300

    task #7 Добавил ссылку контекста MusicPortalContext в файл Program
    Для подключения к базе данных

--- Music_Portal/Program.cs
+++ Music_Portal/Program.cs
@@ -1,4 +1,22 @@
+using Microsoft.EntityFrameworkCore;
+using Music_Portal.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(10); // Длительность сеанса (тайм-аут завершения сеанса)
+    options.Cookie.Name = "Session"; // Каждая сессия имеет свой идентификатор, который сохраняется в куках.
+
+});
+
+// Получаем строку подключения из файла конфигурации
+string? connection = builder.Configuration.GetConnectionString("DefaultConnection");
+
+// добавляем контекст ApplicationContext в качестве сервиса в приложение
+builder.Services.AddDbContext<MusicPortalContext>(options => options.UseSqlServer(connection));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
