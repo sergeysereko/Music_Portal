ea632542414d63149e15241b78772be361105cf2
sergey <sergeysereko@gmail.com>
2023-09-10 22:50:11 +0300

    task #5 Добаил контекстный файл MusicPortalContext
    Для создания базы данных

---
+++ Music_Portal/Models/MusicPortalContext.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+namespace Music_Portal.Models
+{
+    public class MusicPortalContext: DbContext
+    {
+        public MusicPortalContext(DbContextOptions<MusicPortalContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<Music_file> Music_file { get; set; }
+        public DbSet<User> User { get; set; }
+        public DbSet<Style> Style { get; set; }
+        public DbSet<Singer> Singer { get; set; }
+    }
+}
+
+
