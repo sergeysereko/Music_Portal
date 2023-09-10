8a95eba22b18a20fb1b4ea37da8a3074564d0814
sergey <sergeysereko@gmail.com>
2023-09-10 22:00:15 +0300

    task #1 Добавил доменую модель класс Style
    Необходим для хранения в базе данных стиля музыки

---
+++ Music_Portal/Models/Style.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Music_Portal.Models
+{
+    public class Style
+    {
+        public Style()
+        {
+            this.Music_Item = new HashSet<Music_Item>();
+        }
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public ICollection<Music_Item> Music_Item { get; set; }
+    }
+}
