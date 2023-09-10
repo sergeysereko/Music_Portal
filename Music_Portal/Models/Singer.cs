138b2a21510e4aa66c44e330974dbad61a4a1e53
sergey <sergeysereko@gmail.com>
2023-09-10 22:05:54 +0300

    task #2 Добавил доменную можель класс Singer
    Для хранения исполнителя песни в базе данных

---
+++ Music_Portal/Models/Singer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Music_Portal.Models
+{
+    public class Singer
+    {
+        public Singer()
+        {
+            this.Music_Item = new HashSet<Music_Item>();
+        }
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public ICollection<Music_Item> Music_Item { get; set; }
+    }
+}
