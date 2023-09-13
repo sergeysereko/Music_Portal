2889f6975a24eb20bdba66789057cada960d7f8c
sergey <sergeysereko@gmail.com>
2023-09-13 20:20:46 +0300

    task #10 Добавил LoginController Добавил вьюшку Registration
    Необходимы для регистрации пользовтеля

---
+++ Music_Portal/Controllers/LoginController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Music_Portal.Controllers
+{
+    public class LoginController : Controller
+    {
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+    }
+}
