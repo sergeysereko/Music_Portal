b08a10391c1a013d635bdea3f3b6ed504505c8eb
sergey <sergeysereko@gmail.com>
2023-09-13 20:56:15 +0300

    task #11 Добавил метод Registration в контроллер LoginController
    Необходим для обработки и подготовки данных для внесения в таблицу Users

--- Music_Portal/Controllers/LoginController.cs
+++ Music_Portal/Controllers/LoginController.cs
@@ -1,14 +1,76 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Music_Portal.Models;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace Music_Portal.Controllers
 {
     public class LoginController : Controller
     {
 
+        MusicPortalContext db;
+        public LoginController(MusicPortalContext context)
+        {
+            db = context;
+        }
+
+
         [HttpGet]
         public IActionResult Login()
         {
             return View();
+        }
+
+
+        [HttpGet]
+        public IActionResult Registration()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Registration(Registration reg)
+        {
+            if (ModelState.IsValid)
+            {
+                User user = new User();
+                user.Name = reg.Login;
+                user.Email = reg.Email;
+                user.Access = 0;
+
+                byte[] saltbuf = new byte[16];
+
+                RandomNumberGenerator randomNumberGenerator = RandomNumberGenerator.Create();
+                randomNumberGenerator.GetBytes(saltbuf);
+
+                StringBuilder sb = new StringBuilder(16);
+                for (int i = 0; i < 16; i++)
+                    sb.Append(string.Format("{0:X2}", saltbuf[i]));
+                string salt = sb.ToString();
+
+                //переводим пароль в байт-массив  
+                byte[] password = Encoding.Unicode.GetBytes(salt + reg.Password);
+
+                //создаем объект для получения средств шифрования  
+                var md5 = MD5.Create();
+
+                //вычисляем хеш-представление в байтах  
+                byte[] byteHash = md5.ComputeHash(password);
+
+                StringBuilder hash = new StringBuilder(byteHash.Length);
+                for (int i = 0; i < byteHash.Length; i++)
+                    hash.Append(string.Format("{0:X2}", byteHash[i]));
+
+                user.Password = hash.ToString();
+                user.Salt = salt;
+                db.Users.Add(user);
+                db.SaveChanges();
+                return RedirectToAction("Login");
+            }
+
+            return View(reg);
         }
 
     }
