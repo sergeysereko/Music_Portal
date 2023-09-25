ae45dfe01785dd30d5fe14a37e26aaff8434f7f7
sergey <sergeysereko@gmail.com>
2023-09-25 20:18:41 +0300

    task #30 перенес контекстную модель MusicPortalContext на слой DAL
    Необходим для создания базы данных

---
+++ MusicPortal.DAL/Context/MusicPortalContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MusicPortal.DAL.Models;
+
+namespace MusicPortal.DAL.Context
+{
+    public class MusicPortalContext : DbContext
+    {
+        public MusicPortalContext(DbContextOptions<MusicPortalContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<Music_file> Music_files { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Style> Styles { get; set; }
+        public DbSet<Singer> Singers { get; set; }
+    }
+}
