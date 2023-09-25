cfeabf30e095734fa4dff3888d5be90744b822d0
sergey <sergeysereko@gmail.com>
2023-09-25 22:08:31 +0300

    task #32 Добавил интерфейс IUnitOfWorks
    Необходим для реализации репозитория для моделей базых данных

---
+++ MusicPortal.DAL/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using MusicPortal.DAL.Interfaces;
+using MusicPortal.DAL.Models;
+
+namespace MusicPortal.DAL.Interfaces
+{
+    public interface IUnitOfWork
+    {
+        IRepository<Music_file> Music_files { get; }
+        IRepository<Singer> Singers { get; }
+        IRepository<Style> Styles { get; }
+        IRepository<User> Users {  get; }
+        Task Save();
+    }
+}
