8bda0d64ce8d96fc79b9689ddca71cd407660252
sergey <sergeysereko@gmail.com>
2023-09-26 22:41:28 +0300

    task #35 Добавил репозиторий SingerRepository
    Для раелизации CRUD модели Singer

--- MusicPortal.DAL/Repositories/SingerRepository.cs
+++ MusicPortal.DAL/Repositories/SingerRepository.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace MusicPortal.DAL.Repositories
 {
@@ -18,5 +19,41 @@ namespace MusicPortal.DAL.Repositories
         {
             this.db = context;
         }
+
+        public async Task<IEnumerable<Singer>> GetAll()
+        {
+            return await db.Singers.ToListAsync();
+        }
+
+        public async Task<Singer> Get(int id)
+        {
+            Singer? singer = await db.Singers.FindAsync(id);
+            return singer;
+        }
+
+        public async Task<Singer> Get(string name)
+        {
+            var singers = await db.Singers.Where(a => a.Name == name).ToListAsync();
+            Singer? singer = singers?.FirstOrDefault();
+            return singer;
+        }
+
+        public async Task Create(Singer singer)
+        {
+            await db.Singers.AddAsync(singer);
+        }
+
+        public void Update(Singer singer)
+        {
+            db.Entry(singer).State = EntityState.Modified;
+        }
+
+        public async Task Delete(int id)
+        {
+            Singer? singer = await db.Singers.FindAsync(id);
+            if (singer != null)
+                db.Singers.Remove(singer);
+        }
+
     }
 }
