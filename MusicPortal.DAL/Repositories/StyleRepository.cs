8ba169666067e9d8837bfaee92960450dd69d22f
sergey <sergeysereko@gmail.com>
2023-09-26 22:25:13 +0300

    task #34 Добавил репозиторий StyleRepository
    Для реализации CRUD для модели Style

--- MusicPortal.DAL/Repositories/StyleRepository.cs
+++ MusicPortal.DAL/Repositories/StyleRepository.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace MusicPortal.DAL.Repositories
 {
@@ -18,5 +19,42 @@ namespace MusicPortal.DAL.Repositories
         {
             this.db = context;
         }
+
+        public async Task<IEnumerable<Style>> GetAll()
+        {
+            return await db.Styles.ToListAsync();
+        }
+
+        public async Task<Style> Get(int id)
+        {
+            Style? style = await db.Styles.FindAsync(id);
+            return style;
+        }
+
+        public async Task<Style> Get(string name)
+        {
+            var styles = await db.Styles.Where(a => a.Name == name).ToListAsync();
+            Style? style = styles?.FirstOrDefault();
+            return style;
+        }
+
+        public async Task Create(Style style)
+        {
+            await db.Styles.AddAsync(style);
+        }
+
+        public void Update(Style style)
+        {
+            db.Entry(style).State = EntityState.Modified;
+        }
+
+        public async Task Delete(int id)
+        {
+            Style? style = await db.Styles.FindAsync(id);
+            if (style != null)
+                db.Styles.Remove(style);
+        }
+
+
     }
 }
