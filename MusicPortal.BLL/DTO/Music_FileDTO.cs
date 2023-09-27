92e3c75f6bc9a688d37496efc0f1afc6fcb41ecd
sergey <sergeysereko@gmail.com>
2023-09-27 19:55:16 +0300

    #task #42 Добавил транферный класс Music_FileDTO в средний слой BLL
    Необходим для связи с нижним слоем DAL

---
+++ MusicPortal.BLL/DTO/Music_FileDTO.cs
@@ -0,0 +1,40 @@
+﻿using MusicPortal.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicPortal.BLL.DTO
+{
+    public class Music_FileDTO
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Поле должно быть установлено.")]
+        [Display(Name = "Название: ")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Поле должно быть установлено.")]
+        [Display(Name = "Размер: ")]
+        public string? Size { get; set; }
+        [Required(ErrorMessage = "Поле должно быть установлено.")]
+        [Display(Name = "Рейтинг: ")]
+        public int Rating { get; set; }
+        [Required(ErrorMessage = "Поле должно быть установлено.")]
+        [Display(Name = "Путь к файлу: ")]
+        public string? File { get; set; }
+
+        public int Id_User { get; set; }
+
+        public int Id_Singer { get; set; }
+
+        public int Id_Style { get; set; }
+
+        public string Style { get; set; }
+
+        public string Singer { get; set; }
+
+        public string User { get; set; }
+
+    }
+}
