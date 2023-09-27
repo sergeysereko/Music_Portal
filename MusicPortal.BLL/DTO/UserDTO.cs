3a1c62081f9477db67a045fd57de010be52321cd
sergey <sergeysereko@gmail.com>
2023-09-27 19:29:47 +0300

    task # 38 Добавил модель биснес слоя UserDTO
    Для связи с слоем DAL

---
+++ MusicPortal.BLL/DTO/UserDTO.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicPortal.BLL.DTO
+{
+    public class UserDTO
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Поле должно быть установлено.")]
+        [Display(Name = "Логин: ")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Поле должно быть установлено.")]
+        [Display(Name = "Email: ")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Поле должно быть установлено")]
+        [Display(Name = "Пароль: ")]
+        public string Password { get; set; }
+        public string Salt { get; set; }
+
+        public int Access { get; set; }
+
+    }
+}
