4b1a0efb520433730c48a2b4746a4462ed64de9d
sergey <sergeysereko@gmail.com>
2023-09-27 19:39:56 +0300

    task #39
    Добавил модель StyleDTO
    
    Для связи с нижним слоем DAL

---
+++ MusicPortal.BLL/DTO/StyleDTO.cs
@@ -0,0 +1,22 @@
+﻿using MusicPortal.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicPortal.BLL.DTO
+{
+    public class StyleDTO
+    {
+       
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Поле должно быть установлено.")]
+        [Display(Name = "Название: ")]
+        public string Name { get; set; }
+
+        
+    }
+}
