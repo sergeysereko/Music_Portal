9d01304eb7d2ab9315dfb0f5892d128ce7966529
sergey <sergeysereko@gmail.com>
2023-09-27 20:17:35 +0300

    #task 43 Добавил папку Interfaces Добавил интерфейс IUserService

---
+++ MusicPortal.BLL/Interfaces/IUserService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MusicPortal.BLL.DTO;
+
+namespace MusicPortal.BLL.Interfaces
+{
+    public interface IUserService
+    {
+        Task CreateUser(UserDTO userDto);
+        Task UpdateUser(UserDTO teamDto);
+        Task DeleteUser(int id);
+        Task<UserDTO> GetUser(int id);
+        Task<IEnumerable<UserDTO>> GetUsers();
+    }
+}
