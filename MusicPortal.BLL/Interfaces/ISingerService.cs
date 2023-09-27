ed8a60941ed3e503d6a709c83ff2f69b55669be6
sergey <sergeysereko@gmail.com>
2023-09-27 21:34:47 +0300

    task #44 Добавил интерфейс ISingerService

---
+++ MusicPortal.BLL/Interfaces/ISingerService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MusicPortal.BLL.DTO;
+
+namespace MusicPortal.BLL.Interfaces
+{
+    public interface ISingerService
+    {
+        Task CreateSinger(SingerDTO singerDto);
+        Task UpdateSinger(SingerDTO singerDto);
+        Task DeleteSinger(int id);
+        Task<SingerDTO> GetSinger(int id);
+        Task<IEnumerable<SingerDTO>> GetSingers();
+    }
+}
