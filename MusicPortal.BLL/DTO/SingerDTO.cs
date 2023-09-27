aaef13e44dd74bbf0ccbb4a6c210482b67265c8b
sergey <sergeysereko@gmail.com>
2023-09-27 19:48:32 +0300

    task #41 Добавил трансферную модель SingerDTO в слой BLL
    Необходим для связи с нижним слоем DAL

---
+++ MusicPortal.BLL/DTO/SingerDTO.cs
@@ -0,0 +1,23 @@
+﻿using MusicPortal.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicPortal.BLL.DTO
+{
+    public class SingerDTO
+    {    
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Поле должно быть установлено.")]
+        [Display(Name = "Название: ")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Поле должно быть установлено.")]
+        [Display(Name = "Постер исполнителя: ")]
+        public string? Poster { get; set; }
+
+       
+    }
+}
