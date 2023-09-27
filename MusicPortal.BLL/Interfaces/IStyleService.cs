670a16f854084dc9917398303ef4776d0d09b833
sergey <sergeysereko@gmail.com>
2023-09-27 21:38:25 +0300

    task #45 Добавил интерфейс IStyleService

---
+++ MusicPortal.BLL/Interfaces/IStyleService.cs
@@ -0,0 +1,18 @@
+﻿using MusicPortal.BLL.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicPortal.BLL.Interfaces
+{
+    public interface IStyleService
+    {
+        Task CreateStyle(StyleDTO styleDto);
+        Task UpdateStyle(StyleDTO styleDto);
+        Task DeleteStyle(int id);
+        Task<SingerDTO> GetStyle(int id);
+        Task<IEnumerable<SingerDTO>> GetStyles();
+    }
+}
