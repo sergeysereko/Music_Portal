a3bf0fcb5490761baeeb4250f956dcd0c676605c
sergey <sergeysereko@gmail.com>
2023-09-27 21:45:21 +0300

    task #46 Добавил интерфейс IMusicFileService

---
+++ MusicPortal.BLL/Interfaces/IMusicFileService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MusicPortal.BLL.DTO;
+
+namespace MusicPortal.BLL.Interfaces
+{
+    public interface IMusicFileService
+    {
+        Task CreateMusicFile(Music_FileDTO mfDto);
+        Task UpdateMusicFile(Music_FileDTO mfDto);
+        Task DeleteMusicFile(int id);
+        Task<Music_FileDTO> GetMusicFile(int id);
+        Task<IEnumerable<Music_FileDTO>> GetMusicFiles();
+    }
+}
