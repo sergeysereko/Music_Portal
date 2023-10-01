56aa26bfc5fa6cea645702fcf0e934fb1bd38e2c
sergey <sergeysereko@gmail.com>
2023-10-01 16:16:57 +0300

    task #51 Добавил папку Infrastructure Добаивл класс MusicPortalContextExtensions

---
+++ MusicPortal.BLL/Infrastructure/MusicPortalContextExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MusicPortal.DAL.Models;
+using System.Runtime.CompilerServices;
+using MusicPortal.DAL.Context;
+
+namespace MusicPortal.BLL.Infrastructure
+{
+    public static class MusicPortalContextExtensions
+    {
+        public static void AddMusicPortalContext(this IServiceCollection services, string connection)
+        {
+            services.AddDbContext<MusicPortalContext>(options => options.UseSqlServer(connection));
+        }
+    }
+}
